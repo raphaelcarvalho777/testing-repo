cbfac98b7a44c21db5160111c597ab67ff662d87
raphaelcarvalho777 <raphael.carvalho@loggi.com>
2023-11-17 10:17:15 -0300

    testing

---
+++ node_modules/openmct/dist/src/api/menu/menu.d.ts
@@ -0,0 +1,22 @@
+export namespace MENU_PLACEMENT {
+    let TOP: string;
+    let TOP_LEFT: string;
+    let TOP_RIGHT: string;
+    let BOTTOM: string;
+    let BOTTOM_LEFT: string;
+    let BOTTOM_RIGHT: string;
+    let LEFT: string;
+    let RIGHT: string;
+}
+export default Menu;
+declare class Menu {
+    constructor(options: any);
+    options: any;
+    dismiss(): void;
+    show(): void;
+    showMenu(): void;
+    showSuperMenu(): void;
+    destroy: any;
+    el: any;
+}
+//# sourceMappingURL=menu.d.ts.map
---
+++ node_modules/openmct/dist/src/api/user/StoragePersistance.d.ts
@@ -0,0 +1,8 @@
+declare const _default: StoragePersistance;
+export default _default;
+declare class StoragePersistance {
+    getActiveRole(): string | null;
+    setActiveRole(role: any): void;
+    clearActiveRole(): void;
+}
+//# sourceMappingURL=StoragePersistance.d.ts.map
